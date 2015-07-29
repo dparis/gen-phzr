0949d3092ce9b77ad20de9bc917fca6793515683
Dylan Paris <dylan.paris+github@gmail.com>
2015-07-29 16:32:28 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gen-phaser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/gen_phaser/core.clj
@@ -0,0 +1,7 @@
+(ns gen-phaser.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
