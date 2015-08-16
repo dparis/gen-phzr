03986d16cefb32cbff3224ae3d32e918bd56caa3
Dylan Paris <dylan.paris+github@gmail.com>
2015-08-15 18:30:42 -0700

    Update license information

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject gen-phzr "1.0.0"
   :description "Clojure app to generate phzr code from Phaser javascript source"
   :url "http://"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]
                  [funcool/cuerdas "0.5.0"]
